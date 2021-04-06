fe97c254d268d3f382287c54b61acfc5460d3656
Khan Tanveer <khantanveer7977@gmail.com>
2021-04-06 20:52:55 +0530

    icons changes and internet access

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:robo_gen/pages/home.dart';
 
 void main() {
@@ -8,6 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Robo Gen",
